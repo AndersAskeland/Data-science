da4149f358fe2004464557c121789ef9b71c7f34
Anders Askeland <ps7x@aas.its.nja.dk>
2020-02-10 09:45:17 +0100

    added a new folder

---
+++ Other/DDA.R.Course/R/setup.R
@@ -0,0 +1,5 @@
+# Setup description -------------------------------------------------------
+#
+# Add functions or other code that are used to setup your files so that all
+# options are in one location, and referenced via a function or `source()` call.
+#
