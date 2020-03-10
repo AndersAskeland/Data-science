010e2966f827bb2aaa3c8f12e94dd660c543cfd8
AndersAskeland <anders_askeland@hotmail.com>
2020-03-10 19:56:27 +0100

    stuff

--- Other/10 days of statistics/Day 1- Quartiles.R
+++ Other/10 days of statistics/Day 1- Quartiles.R
@@ -1,1 +1,12 @@
-day 1 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+data<-read_lines("data_day1_01")
+x <- scan(text = data[1], sep = " ", quiet = TRUE)
+y <- scan(text = data[2], sep = " ", quiet = TRUE)
+
+quantile(y, x)
+xwMultiplied <- x * w 
+sum1 <- sum(xwMultiplied) 
+weightedMean <- (sum1/sum(w)) 
+
+
+cat(trimws(format(round(weightedMean,1), nsmall=1))) # trimws removes trailing white spaces that sometimes causes errors.
